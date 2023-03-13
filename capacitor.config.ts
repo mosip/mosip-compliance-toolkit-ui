4b17696e73f0e47be61749193c0766cc5e99be73
Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>
2023-03-13 19:39:40 +0530

    Revert "MOSIP-26495 added capacitor preferences instead of capacitor cookies"

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,11 @@ const config: CapacitorConfig = {
   appName: 'mosip-compliance-toolkit-ui',
   webDir: 'dist/mosip-compliance-toolkit-ui',
   bundledWebRuntime: false,
+  plugins: {
+    CapacitorCookies: {
+      enabled: true,
+    },
+  },
   // server: {
   //   url: 'http://192.168.1.10:4200',
   //   cleartext: true
