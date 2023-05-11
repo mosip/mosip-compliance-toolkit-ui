5d8fc60f73778865a61bae406e8b99f24c0e6736
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2023-05-11 10:38:45 +0530

    build issue

--- src/app/core/services/abis-testcase-service.ts
+++ src/app/core/services/abis-testcase-service.ts
@@ -209,17 +209,8 @@ export class AbisTestCaseService {
           "requestId": requestId,
           "requesttime": new Date().toISOString(),
           "referenceId": referenceId
-          // "referenceUrl": null,
-          // "gallery": {
-          //   "referenceIds": galleryIds
-          // },
-          // "flags": {
-          //   "maxResults": 0,
-          //   "targetFPIR": 0,
-          //   "flag1": "string",
-          //   "flag2": "string"
-          // }
-      }
+        }
+      }  
     }
     return request;
   }
