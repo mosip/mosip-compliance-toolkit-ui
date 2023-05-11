68889ba20f21bb17e5e6515cb49c01bcc71793d4
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2023-05-11 12:38:36 +0530

    disabled heartbeatOutgoing

--- src/app/core/services/activemq-service.ts
+++ src/app/core/services/activemq-service.ts
@@ -26,7 +26,7 @@ export class ActiveMqService {
       // How often to heartbeat?
       // Interval in milliseconds, set to 0 to disable
       heartbeatIncoming: 0, // Typical value 0 - disabled
-      heartbeatOutgoing: 20000, // Typical value 20000 - every 20 seconds
+      heartbeatOutgoing: 0, // Typical value 20000 - every 20 seconds
       // Wait in milliseconds before attempting auto reconnect
       // Set to 0 to disable
       // Typical value 500 (500 milli seconds)
