a1d3c0c848231ddbdb3f82f8f33d7dd52dd7b4b2
sudeep <sudeep.j7353@gmail.com>
2024-04-05 12:13:39 +0530

    session idle timeout fix.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
-
+import { HostListener } from '@angular/core';
+import { SessionLogoutService } from '../app/core/services/session-logout.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  subscriptions: Subscription[] = [];
   
   title = 'mosip-compliance-toolkit-ui';
-  
-  constructor(
 
+  constructor(
+    private sessionLogoutService: SessionLogoutService
   ) {
   }
 
+  @HostListener('keypress')
+  @HostListener('document:mousedown', ['$event'])
+  @HostListener('document:keypress', ['$event'])
+  onMouseClick() {
+    this.sessionLogoutService.setisActive(true);
+  }
+
   ngOnInit() {
+    this.subscriptions.push(this.sessionLogoutService.currentMessageAutoLogout.subscribe(() => { }));
+    this.sessionLogoutService.changeMessage({ timerFired: false });
   }
   ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
